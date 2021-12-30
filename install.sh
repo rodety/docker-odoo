f38429bc07c48e48044abda4083b42aea2b4765f
Your Name <you@example.com>
2021-12-30 10:54:53 -0500

    instalando en server controlado

---
+++ install.sh
@@ -0,0 +1,27 @@
+#/bash/bin
+
+sudo apt-get -y update
+sudo apt-get -y install openssh-server vim
+
+sudo apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
+
+#para editar los hosts
+#sudo vim /etc/hosts
+#para trasmitir mixx
+#sudo add-apt-repository ppa:mixxx/mixxx
+#sudo apt update
+#sudo apt install mixxx
+
+
